492da9d12279bc824621cbe3e5ab4a5b7a174ba1
isaacasher97 <isaac.asher97@gmail.com>
2023-07-23 23:51:44 -0400

    First Commit, React Setup

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+// the component function
+const Footer = (props) => {
+
+    //The Components Returned JSX
+    return <div className="ComponentName"> This is ComponentName</div>
+}
+
+// export the component
+export default Footer
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+// the component function
+const Main = (props) => {
+
+    //The Components Returned JSX
+    return <div className="ComponentName"> This is ComponentName</div>
+}
+
+// export the component
+export default Main
